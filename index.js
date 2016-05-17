007bd77b8b8b138e43910c6a200de61a5ba2c786
Anubhav Gupta <anubhav200@gmail.com>
2016-05-17 13:24:36 +0530

    registering bot

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-/**
- * Created by suman on 16/05/16.
- */
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
 
+app.set('port', (process.env.PORT || 5000))
 
-var C = require('chanakya'),
-  Cfb = require('chanakya-facebook');
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
 
+// Process application/json
+app.use(bodyParser.json())
 
-var bot = C.bootstrap({
-  mount: 'bot',
-  expectation: 'greetings',
-  token: 'EAALfXGCDGs0BABTSEqfhAldWyiamK5iAYOiFMkquTxSedHoGocIsJH4jsTPnoSU8oPPWC1qUvYSX54JCl2bjZCyX2201d94uYTMCrQyL0q4sZBrElPgsn5ETJJ5fQU7BzkLWfTRUyunXKKd5K3ZCZAdpZAZB0FIrLSkLbTBY6IRwZDZD'
-});
+// Index route
+app.get('/', function (req, res) {
+  res.send('Hello world, I am a chat bot')
+})
 
-Cfb.init(bot);
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+  console.log('running on port', app.get('port'))
+})
